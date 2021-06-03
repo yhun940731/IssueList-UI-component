7061313e6aa369738d93d085981905f0e2537771
yhun940731@gmail.com <yhun940731@gmail.com>
2021-06-03 16:17:46 +0900

    [추가] issue list markUp

--- src/containers/Lists.js
+++ src/containers/Lists.js
@@ -9,8 +9,14 @@ export default function Lists({ data }) {
             <li key={issue.id}>
               <a href={`/issue.id`}>
                 <h2>
-                  #{issue.id} {issue.title}
+                  #{issue.number} {issue.title}
                 </h2>
+                <div>
+                  작성자: {issue.user.login}, 작성일: {issue.created_at}
+                </div>
+                <span>코멘트: {issue.comments}</span>
+                {/* 작성자, 작성일 */}
+                {/* 코멘트 */}
               </a>
             </li>
           ))}
